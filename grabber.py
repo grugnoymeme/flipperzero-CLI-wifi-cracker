dab7218fdc641dab7460873e1dcfa995b08c7362
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-07-02 19:33:27 +0200

    Update grabber.py
    TOOL UPDATED 100% WORKING

--- grabber.py
+++ grabber.py
@@ -1,20 +1,11 @@
-import pyshark
 import subprocess
 
-# Select the input .pcap file
+# Select input .pcap file
 input_file = input("Insert input .pcap file's name: ")
 
-# Create a new file .pcap conteining only EAPOL handshakes
-output_file = "eapol.pcap"
-capture = pyshark.FileCapture(input_file, display_filter="eapol", output_file=output_file)
-
 # Convert the .pcap file in .hc22000
-hc22000_file = "eapol.hc22000"
-subprocess.run(["tshark", "-r", output_file, "-Y", "eapol", "-w", hc22000_file])
+hc22000_file = "wpa_crack.hc22000"
+subprocess.run(["hcxpcapngtool", "-o", hc22000_file, input_file])
 
-# Import the .hc22000 file into hashcat and save the results on a new file "risultati.txt"
-results_file = "risultati.txt"
-subprocess.run(["hashcat", "-m", "22000", hc22000_file, "wordlist.txt", "-o", results_file])
-
-# Print a "Success" message
-print("You'll find results in " + results_file)
+# Import the .hc22000 file into hashcat, find out the password and show it
+subprocess.run(["hashcat", "-m", "22000", hc22000_file, "wordlist.txt", "--show"])
