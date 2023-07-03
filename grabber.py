7d9fe0504f355a9c80e08542a43225ee116aeb5a
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-07-03 13:01:14 +0200

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -19,14 +19,26 @@ print("")
 print("")
 
 # Select input .pcap file
-input_file = input("Insert input .pcap file's NAME or PATH: ")
+input_file = ""
+while True:
+    input_file = input("Insert input .pcap file's NAME or PATH: ")
+    if input_file.endswith((".pcap", ".cap", ".pcapng")) and os.path.isfile(input_file):
+        break
+    else:
+        print("Invalid input file. Please make sure the file exists and has a valid format.")
 
 # Convert the .pcap file into .hc22000
 hc22000_file = "wpa_crack.hc22000"
 subprocess.run(["hcxpcapngtool", "-o", hc22000_file, input_file])
 
 # Prompt for wordlist file
-wordlist_file = input("Now insert wordlist file's NAME or PATH: ")
+wordlist_file = ""
+while True:
+    wordlist_file = input("Now insert wordlist file's NAME or PATH: ")
+    if wordlist_file.endswith(".txt") and os.path.isfile(wordlist_file):
+        break
+    else:
+        print("Invalid wordlist file. Please make sure the file exists and has a valid format.")
 
 # Import the .hc22000 file into hashcat, find out the password, and show it
 subprocess.run(["hashcat", "-m", "22000", hc22000_file, wordlist_file, "--show"])
